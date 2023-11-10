11819308423f8bd4d0e085a34b2e9ce5e64113f9
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-10 07:21:05 -0500

    Update main.py
    updated word changing capabilities and set a timer to switch to the translation

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from tkinter import *
 import random
 import pandas as pd
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- CARD GENERATOR ------------------------------- #
 data_file = pd.read_csv('../data/french_words.csv')
-french_dict = data_file.to_dict()
+french_dict = data_file.to_dict(orient='records')
 
 
 def change_word():
-    global study_word
-    new_num = random.randint(0, 100)
-    canvas.delete(study_word)
-    study_word = canvas.create_text(400, 263, text=french_dict['French'][new_num], font=('Ariel', 60, 'bold'))
+    new_word = random.choice(french_dict)
+    canvas.itemconfig(card_background, image=card_front)
+    canvas.itemconfig(card_title, text='French')
+    canvas.itemconfig(study_word, text=new_word['French'])
+    window.update()
+    time.sleep(3)
+    canvas.itemconfig(card_background, image=card_back)
+    canvas.itemconfig(card_title, text='English')
+    canvas.itemconfig(study_word, text=new_word['English'])
 
 
 # ----------------------------- SAVE RESULTS -------------------------------- #
@@ -27,13 +33,13 @@ card_front = PhotoImage(file='../images/card_front.png')
 card_back = PhotoImage(file='../images/card_back.png')
 
 canvas = Canvas(width=800, height=550)
-canvas.create_image(2, 1, image=card_front, anchor='nw')
+card_background = canvas.create_image(2, 1, image=card_front, anchor='nw')
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
-canvas.create_text(400, 150, text='French', font=("Ariel", 40, 'italic'))
+card_title = canvas.create_text(400, 150, text='', font=("Ariel", 40, 'italic'))
 
-study_word = canvas.create_text(400, 263, text='trouve', font=('Ariel', 60, 'bold'))
+study_word = canvas.create_text(400, 263, text='', font=('Ariel', 60, 'bold'))
 
 x_image = PhotoImage(file='../images/wrong.png')
 incorrect_button = Button(text='Incorrect', image=x_image, bg=BACKGROUND_COLOR, highlightthickness=0,
@@ -44,5 +50,7 @@ check_image = PhotoImage(file='../images/right.png')
 correct_button = Button(text='Correct', image=check_image, bg=BACKGROUND_COLOR, highlightthickness=0,
                         command=change_word)
 correct_button.grid(column=1, row=1)
+
+change_word()
 
 window.mainloop()
