778cef425e5c54e4e24db94f64838f49ddf2be14
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-11 07:36:32 -0500

    Update main.py
    added flip card function
    
    working on saving results to csv

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 from tkinter import *
 import random
 import pandas as pd
-import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- CARD GENERATOR ------------------------------- #
 data_file = pd.read_csv('../data/french_words.csv')
 french_dict = data_file.to_dict(orient='records')
+current_card = {}
+unknown_card_list = []
 
 
 def change_word():
-    new_word = random.choice(french_dict)
+    global current_card, flip_timer
+    print(current_card)
+    window.after_cancel(flip_timer)
+    current_card = random.choice(french_dict)
     canvas.itemconfig(card_background, image=card_front)
-    canvas.itemconfig(card_title, text='French')
-    canvas.itemconfig(study_word, text=new_word['French'])
-    window.update()
-    time.sleep(3)
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(study_word, text=current_card['French'], fill='black')
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
     canvas.itemconfig(card_background, image=card_back)
-    canvas.itemconfig(card_title, text='English')
-    canvas.itemconfig(study_word, text=new_word['English'])
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(study_word, text=current_card['English'], fill='white')
+    if unknown_button:
+        save_unknown()
 
 
 # ----------------------------- SAVE RESULTS -------------------------------- #
+
+# def remove_known():
+#     french_dict.remove(current_card)
+#
+#
+# def save_unknown():
+#     unknown_card_list.append(current_card)
+#     with open('words_to_learn.csv', 'w') as new_data:
+#         new_data.write(unknown_card_list[-1])
+
+
 # ----------------------------- UI SETUP ------------------------------------ #
 
 window = Tk()
 window.title('Flashy')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 card_front = PhotoImage(file='../images/card_front.png')
 card_back = PhotoImage(file='../images/card_back.png')
@@ -42,14 +63,14 @@ card_title = canvas.create_text(400, 150, text='', font=("Ariel", 40, 'italic'))
 study_word = canvas.create_text(400, 263, text='', font=('Ariel', 60, 'bold'))
 
 x_image = PhotoImage(file='../images/wrong.png')
-incorrect_button = Button(text='Incorrect', image=x_image, bg=BACKGROUND_COLOR, highlightthickness=0,
-                          command=change_word)
-incorrect_button.grid(column=0, row=1)
+unknown_button = Button(text='Incorrect', image=x_image, bg=BACKGROUND_COLOR, highlightthickness=0,
+                        command=change_word)
+unknown_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file='../images/right.png')
-correct_button = Button(text='Correct', image=check_image, bg=BACKGROUND_COLOR, highlightthickness=0,
-                        command=change_word)
-correct_button.grid(column=1, row=1)
+known_button = Button(text='Correct', image=check_image, bg=BACKGROUND_COLOR, highlightthickness=0,
+                      command=change_word)
+known_button.grid(column=1, row=1)
 
 change_word()
 
